1a136589dc11d255cd4cc059f9fe248603fb0bf5
Vlad-Lab319 <solo15mag@gmail.com>
2022-02-04 03:51:05 +0000

    Add user controller and update routes

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   resources :products, only: [:index, :show]
   resources :categories, only: [:show]
   resources :about, only: [:index]
+  resources :user, only: [:new, :create]
 
   resource :cart, only: [:show] do
     post   :add_item
@@ -18,6 +19,8 @@ Rails.application.routes.draw do
     resources :products, except: [:edit, :update, :show]
     resources :categories, except: [:edit, :update]
   end
+
+
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
