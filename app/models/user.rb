8774a9e1328a7a5a0d702715b57adea321fba04e
Vlad-Lab319 <solo15mag@gmail.com>
2022-02-08 03:26:29 +0000

    Add basic user specs and update user model validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,12 @@
 class User < ActiveRecord::Base
   has_secure_password
 
+  validates :name,                  presence: true
+  validates :last_name,             presence: true
+  validates :email,                 presence: true
+  validates :password,              length: { minimum: 8 }
+  validates :password_confirmation, presence: true
+
   def self.authenticate_with_credentials(email, password)
     user = User.find_by_email(email)
     user && user.authenticate(password)
