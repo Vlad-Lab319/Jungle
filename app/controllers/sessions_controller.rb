f67876480288cb28bbaf581b1f535ba4934e863a
Vlad-Lab319 <solo15mag@gmail.com>
2022-02-08 01:33:59 +0000

    Add authenticate_with_credentials to user model and update sessions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,13 +1,11 @@
 class SessionsController < ApplicationController
 
   def create
-    user = User.find_by_email(params[:email])
+    if user = User.authenticate_with_credentials(params[:email], params[:password])
     # If the user exists AND the password entered is correct.
-    if user && user.authenticate(params[:password])
-      # Save the user id inside the browser cookie. This is how we keep the user 
-      # logged in when they navigate around our website.
+    # logged in when they navigate around our website.
       session[:user_id] = user.id
-      redirect_to '/'
+      redirect_to :root
     else
     # If user's login doesn't work, send them back to the login form.
       redirect_to '/login'
